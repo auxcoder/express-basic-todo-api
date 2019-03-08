d1bc2c185e79428a56ae6b5cf057216187c4a660
Pavel Duran <auxcoder@gmail.com>
2019-03-07 20:59:42 -0500

    Add tests for todos CRUD methods

---
+++ routes/todos.test.js
@@ -0,0 +1,99 @@
+// #region
+const app = require('../server');
+const assert = require('assert');
+// .agent(app.listen()) is used when the --watch flag is used with Mocha.
+// but only when I ran that watching command as a npm script from package.json.
+const request = require('supertest').agent(app.listen());
+var chance = require('chance').Chance();
+const todoObj = {
+  title: chance.sentence({ words: 5 }),
+  completed: chance.bool({ likelihood: 30 })
+};
+let testId;
+// #endregion
+// READ
+describe('GET /todos', () => {
+  it('should get all todos records', done => {
+    request
+      .get('/api/todos')
+      // by default
+      // .set('Accept', 'application/json')
+      .expect(200)
+      .expect(res => {
+        assert.equal(res.body.error, false);
+        assert.equal(res.body.data.length > 0, true);
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+// CREATE
+describe('POST /todos', () => {
+  it('should create a new Todo', done => {
+    request
+      .post('/api/todos')
+      .send(todoObj)
+      .expect(201)
+      .expect(res => {
+        testId = res.body.data.id;
+        // check that the source data is in the res
+        Object.keys(todoObj).forEach(key => {
+          assert.equal(todoObj[key], res.body.data[key]);
+        });
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+// READ one
+describe('GET /todos/<id>', () => {
+  it('should get one record', done => {
+    request
+      .get(`/api/todos/${testId}`)
+      .expect(200)
+      .expect(res => {
+        Object.keys(todoObj).forEach(key => {
+          assert.equal(todoObj[key], res.body.data[key]);
+        });
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+// UPDATE
+describe('PATCH /todos/<id>', () => {
+  it('should update a todo record', done => {
+    request
+      .patch(`/api/todos/${testId}`)
+      .send(Object.assign(todoObj, { completed: true }))
+      .expect(200)
+      .expect(res => {
+        assert.equal(res.body.error, false);
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
+describe('DELETE /todos/<id>', () => {
+  it('should remove a todo record', done => {
+    request
+      .delete(`/api/todos/${testId}`)
+      .expect(200)
+      .expect(res => {
+        assert.equal(res.body.error, false);
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
