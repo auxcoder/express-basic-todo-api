70c9c4dfc9ed64977c13b96a6de6f488c3a9ae43
Pavel Duran <auxcoder@gmail.com>
2019-03-27 16:54:15 -0400

    Add get user by id

--- routes/controllers/users.js
+++ routes/controllers/users.js
@@ -8,4 +8,19 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(500).json({ errors: [err.message], data: {} }));
 });
 
+// READ
+router.get('/:id([0-9]+)', (req, res) => {
+  if (!req.params.id) console.error('quote ID is required');
+  Users.where('id', req.params.id)
+    .fetch()
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ errors: true, data: {} });
+      } else {
+        res.json({ errors: false, data: data });
+      }
+    })
+    .catch(err => res.status(500).json({ errors: [err.message], data: {} }));
+});
+
 module.exports = router;
