55b8ac24f1ff8d058e604ffa089d2e9cb7363cc4
Pavel Duran <auxcoder@gmail.com>
2019-03-28 12:01:38 -0400

    Add Visibility plugin to User model in order of blacklist user attributes

--- db/models/users.js
+++ db/models/users.js
@@ -1,5 +1,9 @@
 var bookshelf = require('../../bookshelf');
+bookshelf.plugin('visibility');
+
 var Users = bookshelf.Model.extend({
   tableName: 'users',
+  hidden: ['password', 'salt', 'email_verified', 'verification_token'],
 });
+
 module.exports = Users;
