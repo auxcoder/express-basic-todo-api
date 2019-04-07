889f8d8f1a46fbad44ce0153d15273cf21e32286
Pavel Duran <auxcoder@gmail.com>
2019-04-07 18:34:20 -0400

    Extend User model with a custom method findByEmail

--- db/models/users.js
+++ db/models/users.js
@@ -1,12 +1,26 @@
 let Bookshelf = require('./bookshelf');
 require('./tokens');
-var Users = Bookshelf.Model.extend({
-  tableName: 'users',
-  hidden: ['salt', 'verification_token'],
-  softDelete: true,
-  tokens: function() {
-    return this.hasMany('Tokens');
+var Users = Bookshelf.Model.extend(
+  {
+    tableName: 'users',
+    hidden: ['salt', 'verification_token'],
+    softDelete: true,
+    tokens: function() {
+      return this.hasMany('Tokens');
+    },
   },
-});
+  {
+    /**
+     * Find a model based on it's ID
+     * @param {String} email The model's email
+     * @param {Object} [options] Options used of model.fetch
+     * @return {Promise(bookshelf.Model)}
+     */
+    findByEmail: function(email, options) {
+      options = Object.assign({ require: true }, options);
+      return this.forge({ email: email }).fetch(options);
+    },
+  }
+);
 // module
 module.exports = Bookshelf.model('Users', Users);
