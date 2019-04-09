3e85975d42316f7f425b366d5ed3dc3cc616e42d
Pavel Duran <auxcoder@gmail.com>
2019-04-09 17:03:19 -0400

    Add validation for /auth/verify route

--- routes/middlewares/validateAuth.js
+++ routes/middlewares/validateAuth.js
@@ -1,8 +1,16 @@
-const { header } = require('express-validator/check');
+const { header, body } = require('express-validator/check');
 
 module.exports.hasAuthToken = function hasAuthToken(req, res, next) {
   header('authorization', `Authorization header required`).isString();
   header('authorization', `Authorization header malformed`).isBase64();
+
+  var errors = req.validationErrors();
+  if (errors) ifErrors(errors, res);
+  return next();
+};
+
+module.exports.verifyEmail = function hasAuthToken(req, res, next) {
+  body('token', 'Verification token is required').isString();
 
   var errors = req.validationErrors();
   if (errors) ifErrors(errors, res);
