29860a4de9e0ea909b3311a685f265c5d46b3bc9
Pavel Duran <auxcoder@gmail.com>
2019-04-09 17:04:19 -0400

    Make /todos/route authenticated via Password JWT Strategy

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,7 @@ const authRoute = require('./controllers/auth');
 // routes
 require('./misc')(router);
 router.use('/auth', authRoute);
-// router.use('/todos', passport.authenticate('jwt', { session: false }), todosRoute);
-router.use('/todos', todosRoute);
+router.use('/todos', passport.authenticate('jwt', { session: false }), todosRoute);
 router.use('/users', passport.authenticate('jwt', { session: false }), usersRoute);
 // module
 module.exports = router;
