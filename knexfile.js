ac206334e9c8ed4886187a60083693e53db6664a
Pavel Duran <auxcoder@gmail.com>
2019-04-14 14:57:55 -0400

    Use relative path for knex migrations directory

--- knexfile.js
+++ knexfile.js
@@ -4,44 +4,44 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: 'test.db'
+      filename: 'test.db',
     },
     migrations: {
-      directory: `${__dirname}/db/migrations`
+      directory: `${__dirname}/db/migrations`,
     },
     seeds: {
-      directory: `${__dirname}/db/seeds`
+      directory: `${__dirname}/db/seeds`,
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
   },
   staging: {
     client: 'postgresql',
     connection: {
       database: 'stg_db',
       user: 'username',
-      password: 'password'
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      directory: './db/migrations'
-    }
+      directory: `${__dirname}/db/migrations`,
+    },
   },
   production: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
       user: 'username',
-      password: 'password'
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      directory: './db/migrations'
-    }
-  }
+      directory: `${__dirname}/db/migrations`,
+    },
+  },
 };
